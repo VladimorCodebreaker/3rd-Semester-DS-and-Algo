9425ec297d926b6dd8181ae0e98403733c6e3dda
Vladimir Schmadlak <91912841+VladimorCodebreaker@users.noreply.github.com>
2022-10-17 16:10:14 +0300

    Add files via upload

---
+++ DS_and_Algo_3/DS_and_Algo_3/Exercises.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace DS_and_Algo_3
+{
+    public class Exercises
+    {
+        internal static bool IsInRange(double n, double min, double max)
+        {
+            return (min <= n && n <= max);
+        }
+
+        internal static double Find(List<double> numbers, double min, double max)
+        {
+            if ((max < numbers[0]) || (min > numbers[numbers.Count - 1]))
+            {
+                return double.MinValue;
+            }
+
+            int lowIndex = 0;
+            int highIndex = numbers.Count - 1;
+
+            while (true)
+            {
+                if (highIndex - lowIndex == 1)
+                {
+                    if (IsInRange(numbers[highIndex], min, max))
+                    {
+                        return numbers[highIndex];
+                    }
+                    else if (IsInRange(numbers[lowIndex], min, max))
+                    {
+                        return numbers[lowIndex];
+                    }
+                    else
+                    {
+                        return double.MinValue;
+                    }
+                }
+
+                int midIndex = (highIndex + lowIndex) / 2;
+                double toTest = numbers[midIndex];
+
+                if (IsInRange(toTest, min, max))
+                {
+                    return toTest;
+                }
+                if (toTest < min)
+                {
+                    lowIndex = midIndex;
+                }
+                else
+                {
+                    highIndex = midIndex;
+                }
+            }
+        }
+    }
+}
+
---
+++ DS_and_Algo_3/DS_and_Algo_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DS_and_Algo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Commenty
+            // Practice Lambda expressions
+            // C# Homework in C++! ::  Big file -> Split file into several smaller pieces -> iterate over them -> sort them -> merge them
+            // Binary search
+            #endregion
+
+            string[] all = File.ReadAllLines("/Users/vladimirschmadlak/Documents/Code/University/Semester 3/DS and Algo/DS_and_Algo_3/DS_and_Algo_3/numbers.txt");
+            List<double> numbers = new List<double>();
+
+            foreach (string s in all)
+            {
+                numbers.Add(double.Parse(s));
+            }
+
+            Console.WriteLine(Exercises.Find(numbers, 900, 900));
+            Console.ReadKey();
+        }
+    }
+}
