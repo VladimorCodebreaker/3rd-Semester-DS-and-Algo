a35b0b765a6b76b8fc711fa16f12c82d244138a2
Vladimir Schmadlak <91912841+VladimorCodebreaker@users.noreply.github.com>
2022-10-18 12:03:43 +0300

    Homework || Lecture 4

---
+++ DS_and_Algo_4/DS_and_Algo_4/Exercises.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DS_and_Algo_4
+{
+    public class Exercises
+    {
+        internal static bool Checker(char[] sequence, Stack<char> finalStack)
+        {
+            foreach (var element in sequence)
+            {
+                if (element == '(' || element == '[' || element == '{')
+                {
+                    finalStack.Push(element);
+                }
+                else if (element == ')' || element == ']' || element == '}')
+                {
+                    if (finalStack.Count == 0) return false;
+
+                    var topString = finalStack.Pop();
+                    var str = $"{topString}{element}";
+
+                    if (str != "[]" && str != "{}" && str != "()") return false;
+                }
+                else return false;
+            }
+
+            return true;
+        }
+
+        internal static void ReverseList(List<int> list, int start, int end)
+        {
+            if (list.Count == 0 || list.Count == 1) throw new InvalidDataException();
+            if (start >= end)
+            {
+                foreach (var element in list) Print(element);
+                return;
+            }
+
+            int temp = list[start];
+            list[start] = list[end];
+            list[end] = temp;
+
+            ReverseList(list, start + 1, end - 1);
+        }
+
+        private static void Print(int element) => Console.WriteLine(element);
+    }
+}
+
---
+++ DS_and_Algo_4/DS_and_Algo_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+namespace DS_and_Algo_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Check for brackets, using a stack
+        //char[] sequence = new char[] { '(', '{', '[', ']', '}', ')' };
+        //Stack<char> finalStack = new Stack<char>();
+
+        //Console.WriteLine(Exercises.Checker(sequence, finalStack));
+
+        // Reverse a list, using a stack
+        List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Exercises.ReverseList(list, 0, list.Count - 1);
+    }
+}
